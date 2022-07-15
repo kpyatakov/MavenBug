65302b3e780306919299362af1621c442ad4750a
КириллПятаков <kpyatakov@gmail.com>
2022-07-15 23:47:31 +0700

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    //long amount = 1000_60;
-    //boolean registered = true;
+        //long amount = 1000_60;
+        //boolean registered = true;
 
-    long bonus = service.calculate(1000_60,true);
-    System.out.println(bonus);
-  }
+        long bonus = service.calculate(1000_60, true);
+        System.out.println(bonus);
+        
+    }
 }
 
